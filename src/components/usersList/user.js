ec48e990318d5fcc33c9282241e8db098c742752
Gnel <gnelminasyan@gmail.com>
2018-10-29 14:57:10 +0400

    remove node_modules and dist for gitignore update

--- src/components/usersList/user.js
+++ src/components/usersList/user.js
@@ -1,3 +1,8 @@
+import Store from "../../stores";
+
 const setUser = () => {
     const usersList = document.getElementById("users-list");
+    Store.addListener("usersList", (data) => {
+        console.error("++++++++++++++++++++++")
+    });
 }
